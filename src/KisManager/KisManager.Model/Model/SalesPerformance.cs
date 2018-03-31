743cb5d53e81c75fc6e4a09c7dcd3302895cc099
张东奇 <229860255@qq.com>
2018-04-01 01:39:33 +0800

    修改框架。

--- src/KisManager/KisManager.Model/Model/SalesPerformance.cs
+++ src/KisManager/KisManager.Model/Model/SalesPerformance.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KisManager.Model
+﻿namespace KisManager.Model
 {
     /// <summary>
     /// 区域销售业绩。
