2e4da83b56738b22acff7385d66b6439353073b5
张东奇 <229860255@qq.com>
2018-04-01 01:35:42 +0800

    金蝶调用。

---
+++ src/KisManager/KisTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace KisTest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var type = Type.GetTypeFromProgID("K3Login.ClsLogin");
+            Console.WriteLine("type = " + type);
+            object a = Activator.CreateInstance(type);
+            Console.WriteLine("a = " + a);
+            try
+            {
+                var b = type.InvokeMember("[DispID=1610809350]", System.Reflection.BindingFlags.InvokeMethod, null, a, null);
+                Console.WriteLine(b);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("err:"+e);
+            }
+            Console.Read();
+        }
+    }
+}
