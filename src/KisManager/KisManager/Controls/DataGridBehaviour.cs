e228ca76a266056db69ec2cdb4dfde97d7437cff
张东奇 <zhangdq@hzmr-tech.com>
2018-04-03 17:48:31 +0800

    Demo 完成。

---
+++ src/KisManager/KisManager/Controls/DataGridBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace KisManager.Controls
+{
+    public class DataGridBehaviour : Behavior<DataGrid>
+    {
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.LoadingRow += AssociatedObject_LoadingRow;
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.LoadingRow -= AssociatedObject_LoadingRow;
+        }
+
+        private void AssociatedObject_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+    }
+}
--- src/KisManager/KisManager/Views/DlgSaleItemsView.xaml.cs
+++ src/KisManager/KisManager/Views/DlgSaleItemsView.xaml.cs
@@ -24,10 +24,5 @@ namespace KisManager.Views
         {
             InitializeComponent();
         }
-
-        private void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
-        {
-            e.Row.Header = (e.Row.GetIndex()+1).ToString();
-        }
     }
 }
